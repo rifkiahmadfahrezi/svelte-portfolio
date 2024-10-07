af0f97dc0e4cd01a1d19c5b98f6f0d2df762aa56
rikfkiahmadfahrezi <rifkiaf589@gmail.com>
2024-10-07 20:55:23 +0700

    feat: basic layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,21 @@
 <script>
+	import Navbar from '$lib/components/shared/navbar.svelte';
+	import Footer from '$lib/components/shared/footer.svelte';
+
+	import { ModeWatcher } from 'mode-watcher';
+
 	import '../app.css';
+	import { site } from '$lib/site.config';
 </script>
 
-<slot></slot>
+<svelte:head>
+	<title>{site.name} - {site.description}</title>
+	<meta name="description" content={site.description} />
+</svelte:head>
+
+<ModeWatcher defaultMode="system" />
+<Navbar />
+<main class="min-h-svh">
+	<slot />
+</main>
+<Footer />
