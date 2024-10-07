9b62db360d15c1c524433eb359e2f148d8f60213
rikfkiahmadfahrezi <rifkiaf589@gmail.com>
2024-10-07 20:56:58 +0700

    style: fix codes style by prettier

--- svelte.config.js
+++ svelte.config.js
@@ -9,7 +9,7 @@ const config = {
 
 	kit: {
 		alias: {
-			"@/*": "./path/to/lib/*",
+			'@/*': './path/to/lib/*'
 		},
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
