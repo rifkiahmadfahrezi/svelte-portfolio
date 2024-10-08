7622e23aed4d2f7d88fea07720f73ab9c2305267
rikfkiahmadfahrezi <rifkiaf589@gmail.com>
2024-10-09 05:30:38 +0700

    feat: add page title

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -3,7 +3,14 @@
    import Experience from '$lib/components/sections/experience.svelte';
 	import Education from '$lib/components/sections/education.svelte';
 	import Projects from '$lib/components/sections/projects.svelte';
+
+	import { site } from "$lib/site.config";
 </script>
+
+<svelte:head>
+	<title>About | {site.name} - {site.description}</title>
+	<meta name="description" content={site.description} />
+</svelte:head>
 
 <main class="mx-auto w-full max-w-screen-sm px-2 md:max-w-screen-md my-10">
    <Hero />
--- src/routes/blogs/+page.svelte
+++ src/routes/blogs/+page.svelte
@@ -1,11 +1,17 @@
 <script lang='ts' >
 import { formatDate } from "$lib/utils";
+import { site } from "$lib/site.config.js";
 
 export let data
 </script>
 
+<svelte:head>
+	<title>blogs | {site.name} - {site.description}</title>
+	<meta name="description" content={site.description} />
+</svelte:head>
+
 <main class="mx-auto w-full max-w-screen-sm px-2 md:max-w-screen-md my-10" >
-	<h1 class="text-xl md:text-3xl mt-2 mb-5 font-bold" >Writings</h1>
+	<h1 class="text-xl md:text-3xl mt-2 mb-5 font-bold" >Blogs</h1>
 	<ul class="flex flex-col gap-3">
 		{#each data.blogs as blog}
 			<li class="group py-2 px-3 rounded hover:bg-secondary/70 dark:hover:bg-secondary/20"  >
