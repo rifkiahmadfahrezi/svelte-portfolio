ec47fa16fb527b2cf191ccbbbf79906f8e0206ef
rikfkiahmadfahrezi <rifkiaf589@gmail.com>
2024-10-08 20:55:38 +0700

    fetch all blogs to home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Hero from '$lib/components/sections/hero.svelte';
+
+	import { formatDate } from "$lib/utils";
+
+	export let data
+</script>
+
+<div class="mx-auto w-full max-w-screen-sm px-2 md:max-w-screen-md">
+	<Hero />
+	<section class="container mx-auto md:px-5" >
+		<h1 class="text-xl md:text-3xl mt-2 mb-5 font-bold" >Writings</h1>
+		<ul class="flex flex-col gap-3">
+			{#each data.blogs as blog}
+				<li class="group py-2 px-3 rounded hover:bg-secondary/70 dark:hover:bg-secondary/20"  >
+					<a data-sveltekit-preload-data="hover" href={`/blogs/${blog.slug}`}>
+						<h1 class="text-lg md:text-xl font-semibold capitalize group-hover:underline line-clamp-2" >{blog.title}</h1>
+						<p class="text-muted-foreground">{formatDate(blog.date)}</p>
+						<p class="description">{blog.description}</p>
+					</a>
+				</li>
+			{/each}
+		</ul>
+	</section>
+</div>
