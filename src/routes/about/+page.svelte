5217c9fc97a0643821d0c4318250d58ab2c9fd53
rikfkiahmadfahrezi <rifkiaf589@gmail.com>
2024-10-08 20:54:26 +0700

    creeate about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+   import Hero from "$lib/components/sections/hero.svelte";
+   import Experience from '$lib/components/sections/experience.svelte';
+	import Education from '$lib/components/sections/education.svelte';
+	import Projects from '$lib/components/sections/projects.svelte';
+</script>
+
+<main class="mx-auto w-full max-w-screen-sm px-2 md:max-w-screen-md my-10">
+   <Hero />
+	<Experience />
+	<Education />
+	<Projects />
+</main>
